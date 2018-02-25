4f6291aefffdd2e2a940b924e468ee351e3c26b3
Mateo <mateo.kutnjak@fer.hr>
2018-02-25 20:38:21 +0100

    kostur app

---
+++ app/src/main/java/com/example/mateo/photours/Global.java
@@ -0,0 +1,26 @@
+package com.example.mateo.photours;
+
+import android.app.Application;
+import android.os.Environment;
+
+import java.io.File;
+
+/**
+ * Created by mateo on 25/02/2018.
+ */
+
+public class Global extends Application {
+
+    public static final String CLOUD_VISION_API_KEY = "AIzaSyCh-7eNuvSOLM4qFnV5Z3z_UCBM25Mwx0s";
+    public static final String FILE_NAME = "temp.jpg";
+    public static final String ANDROID_CERT_HEADER = "X-Android-Cert";
+    public static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
+
+    public static final int CAMERA_PERMISSIONS_REQUEST = 2;
+    public static final int CAMERA_IMAGE_REQUEST = 3;
+
+    public File getCameraFile() {
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return new File(dir, FILE_NAME);
+    }
+}
